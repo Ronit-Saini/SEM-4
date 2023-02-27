001fcbd47d2c348e2c25612201217f4af3271cac
RonSaini <112055355+RonSaini@users.noreply.github.com>
2023-02-27 16:24:26 +0530

    Add files via upload

---
+++ DAA/WEEK-4/W4Q1.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+int opcount = 0;
+int min[1000], min_value = INT_MAX;
+void printCombinations(int size, int arr[size][size], int buf[size], int buf_1[size], int ix)
+{
+
+    int i, j;
+    if (ix == size)
+    {
+        opcount++;
+        int sum = 0, flag = 1;
+        for (i = 0; i < size; i++)
+            sum += buf[i];
+        for (i = 0; i < size - 1; i++)
+        {
+            for (j = i + 1; j < size; j++)
+            {
+                if (buf_1[i] == buf_1[j])
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+        }
+
+        if (sum < min_value && flag)
+        {
+            for (i = 0; i < size; i++)
+            {
+                min[i] = buf[i];
+            }
+            min_value = sum;
+        }
+    }
+    else
+    {
+        for (i = 0; i < size; i++)
+        {
+            buf[ix] = arr[i][ix];
+            buf_1[ix] = i;
+            printCombinations(size, arr, buf, buf_1, ix + 1);
+        }
+    }
+}
+void main()
+{
+    int i, j, size;
+    printf("Enter the Size of the Square Matrix:\n");
+    scanf("%d", &size);
+    int arr[size][size];
+    printf("Enter Person vs Job Cost Matrix :\n");
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+            scanf("%d", &arr[i][j]);
+    }
+    printf("\n\n");
+
+    printf("Matrix is : \n");
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            printf("%d \t", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    int buff[size];
+    int buff_1[size];
+    printCombinations(size, arr, buff, buff_1, 0);
+
+    printf("\nThe Minimum Cost Combination is : ");
+    for (j = 0; j < size; j++)
+        printf("%d ", min[j]);
+    printf("\nThe Minimum Cost is : %d\n", min_value);
+
+    printf("\n\nOpcount is %d", opcount);
+}
