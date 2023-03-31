5b2846bdbe218054b80ed628f02d624e31ea1761
RonSaini <112055355+RonSaini@users.noreply.github.com>
2023-03-31 16:02:52 +0530

    Add files via upload

--- DBS/WEEK-7( LAB 9 THOUGH)/q5.sql
+++ DBS/WEEK-7( LAB 9 THOUGH)/q5.sql
@@ -22,19 +22,19 @@ begin
     for stud in curs
     loop
     if stud.gpa between 0 and 4 then
-        update student set LetterGrade='F' where current of curs;
+        update studentTable set LetterGrade='F' where current of curs;
     elsif stud.gpa between 4 and 5 then
-        update student set LetterGrade='E' where current of curs;
+        update studentTable set LetterGrade='E' where current of curs;
     elsif stud.gpa between 5 and 6 then
-        update student set LetterGrade='D' where current of curs;
+        update studentTable set LetterGrade='D' where current of curs;
     elsif stud.gpa between 6 and 7 then
-        update student set LetterGrade='C' where current of curs;
+        update studentTable set LetterGrade='C' where current of curs;
     elsif stud.gpa between 7 and 8 then
-        update student set LetterGrade='B' where current of curs;
+        update studentTable set LetterGrade='B' where current of curs;
     elsif stud.gpa between 8 and 9 then
-        update student set LetterGrade='A' where current of curs;
+        update studentTable set LetterGrade='A' where current of curs;
     else
-        update student set LetterGrade='A+' where current of curs;
+        update studentTable set LetterGrade='A+' where current of curs;
     end if;
     end loop;
 end;
