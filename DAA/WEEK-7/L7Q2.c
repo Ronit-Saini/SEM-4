37195d6b200fd0703d9c57ff34ef9097f445783a
RonSaini <112055355+RonSaini@users.noreply.github.com>
2023-04-10 00:15:14 +0530

    Update L7Q2.c

--- DAA/WEEK-7/L7Q2.c
+++ DAA/WEEK-7/L7Q2.c
@@ -1,142 +1,187 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node * Nodeptr;
 struct node
 {
-	struct node *left,*right;
-	int data;
-	int balance;
+    int data;
+    Nodeptr lchild;
+    Nodeptr rchild;
 };
-typedef struct node Node;
+
+Nodeptr getnode()
+{
+    return ((Nodeptr)malloc(sizeof(struct node)));
+}
+
+Nodeptr Insert_BST(Nodeptr root, int item)
+{
+    if (!root)
+    {
+        root = getnode();
+        root->data= item;
+        root->lchild=root->rchild = NULL;
+        return root;
+    }
+    else
+    {
+        if (item<root->data)
+            root->lchild = Insert_BST(root->lchild, item);
+        else if (item>root->data)
+            root->rchild = Insert_BST(root->rchild, item);
+        else
+            printf("Duplicates are not allowed\n");
+        return root;
+    }
+}
+
+Nodeptr create_BST(Nodeptr root,int data)
+{
+    int x;
+    root=getnode();
+    root->data=data;
+    root->lchild=root->rchild=NULL;
+    while(1)
+    {
+        printf("Enter data to insert (-1 to exit) : \t");
+        scanf("%d",&x);
+        if(x==-1)
+        {
+            break;
+        }
+        root=Insert_BST(root,x);
+    }
+    return root;
+}
+
 int max(int a,int b)
 {
-	return a>b? a: b;
+    return a>b?a:b;
 }
-int height(Node *root)
-{
-	if(root==NULL)
-		return 0;
-	return max(height(root->left),height(root->right))+1;
-}
-void balance_factor(Node *root,Node *st[],int *top)
-{
-	if(root==NULL)
-		return;
-	if(root->left==NULL && root->right==NULL)
-	{
-		
-		return;
-	}
-	balance_factor(root->left,st,top);
-	int bf = height(root->left)-height(root->right);
-	root->balance=bf;
-	if(bf>1 || bf<-1)
-		st[++(*top)]=root;
-	balance_factor(root->right,st,top);
-}
-Node *leftrotate(Node *root)
-{
-	Node *z=root;
-	Node *y=root->right;
-	z->right=y->left;
-	y->left=z;
-	return y;
-}
-Node *rightrotate(Node *root)
-{
-	Node *z=root;
-	Node *y=root->left;
-	z->left=y->right;
-	y->right=z;
-	return y;
-}
-Node *avl(Node *root,int key)
-{
-	Node *st[10];
-	int top=-1;
-	balance_factor(root,st,&top);
-	if(top==-1)
-		return root;
-	//search the node
-	//printf("hi\n");
-	Node *snode;
-	if(top==1)
-		snode=st[top-1];
-	else
-		snode=st[top];
-	Node *nd=root;
-	//printf("hi\n");
-	//left case
-	//printf("%d ",snode->data);
-	if(snode->balance>1 && key<nd->left->data)
-		root=rightrotate(snode);
-	//right case
-	else if(snode->balance<-1 && key>nd->right->data)
-		root=leftrotate(snode);
-	//left right case
-	else if(snode->balance>1 && key>nd->left->data)
-	{
-		snode->left =leftrotate(snode->left);
-		root=rightrotate(snode);
-	}
-	//right left case
-	else if(snode->balance<1 && key<nd->right->data)
-	{
-		snode->right =rightrotate(snode->right);
-		root=leftrotate(snode);
-	}
-	return root;
-}
-Node *newNode(int data,Node *root)
-{
-	Node *node=(Node*)malloc(sizeof(Node));
-	node->left=NULL;
-	node->right=NULL;
-	node->data=data;
-	node->balance=0;
-	if(root==NULL)
-	{
-		return node;
-	}
 
-	Node *nd=root;
-	Node *parent;
-	while(nd)
-	{
-		parent=nd;
-		if(nd->data<data)
-			nd=nd->right;
-		else
-			nd=nd->left;
-	}
-	if(parent->data<data)
-		parent->right=node;
-	else
-		parent->left=node;
-	root=avl(root,data);
-	return root;
-}
-void print(Node *root)
+int height(Nodeptr root)
 {
-	if(root==NULL)
-		return;
-	printf("%d ",root->data);
-	print(root->left);
-	print(root->right);
+    if(root)
+    {
+        return max(height(root->lchild),height(root->rchild))+1;
+    }
 }
+
+int balfac(Nodeptr root)
+{
+    return height(root->lchild)-height(root->rchild);
+}
+
+Nodeptr leftrotate(Nodeptr x)
+{
+    Nodeptr y=x->rchild;
+    Nodeptr z=y->lchild;
+
+    y->lchild=x;
+    x->rchild=z;
+
+    return y;
+}
+
+Nodeptr rightrotate(Nodeptr x)
+{
+    Nodeptr y=x->lchild;
+    Nodeptr z=y->rchild;
+
+    y->rchild=x;
+    x->lchild=z;
+    return y;
+}
+
+Nodeptr Insert_AVL(Nodeptr root,int item)
+{
+    if (!root)
+    {
+        root = getnode();
+        root->data= item;
+        root->lchild=root->rchild = NULL;
+        return root;
+    }
+    if (item<root->data)
+            root->lchild = Insert_AVL(root->lchild, item);
+        else if (item>root->data)
+            root->rchild = Insert_AVL(root->rchild, item);
+        else
+            return root;
+    
+    int bal=balfac(root);
+
+    if (bal > 1 && item < root->lchild->data)
+        return rightrotate(root);
+ 
+    if (bal < -1 && item > root->rchild->data)
+        return leftrotate(root);
+
+    if (bal > 1 && item > root->lchild->data)
+    {
+        root->lchild =  leftrotate(root->lchild);
+        return rightrotate(root);
+    }
+ 
+    if (bal < -1 && item < root->rchild->data)
+    {
+        root->rchild = rightrotate(root->rchild);
+        return leftrotate(root);
+    }
+ 
+    return root;
+}
+
+Nodeptr create_AVL(Nodeptr root,int data)
+{
+    int x;
+    root=getnode();
+    root->data=data;
+    root->lchild=root->rchild=NULL;
+    while(1)
+    {
+        printf("Enter data to insert (-1 to exit) : \t");
+        scanf("%d",&x);
+        if(x==-1)
+        {
+            break;
+        }
+        root=Insert_AVL(root,x);
+    }
+    return root;
+}
+
+void preorder(Nodeptr root)
+{
+    if(root)
+    {
+        printf("%d ",root->data);
+        preorder(root->lchild);
+        preorder(root->rchild);
+    }
+}
+
 void main()
 {
-	int n;
-	Node *root=NULL;
-	printf("Enter the Number of Nodes of The AVL Tree : \n");
-	scanf("%d",&n);
-	int x;
-	for(int i=0;i<n;i++)
-	{
-		printf("Enter the %d Node in the AVL Tree \n",(i+1));
-		scanf("%d",&x);
-		root=newNode(x,root);
-	}
-	printf("The AVL Tree Inserted has the Preorder Traversal given by : \n");
-	print(root);
-	printf("\n");
+    Nodeptr root=NULL;
+    int data;
+    printf("Enter Root Data : \t");
+    scanf("%d",&data);
+    root=create_AVL(root,data);
+    printf("\n");
+    preorder(root);
+    printf("\n");
 }
+
+/*
+Enter Root Data :       1
+Enter data to insert (-1 to exit) :     2
+Enter data to insert (-1 to exit) :     3
+Enter data to insert (-1 to exit) :     4
+Enter data to insert (-1 to exit) :     5
+Enter data to insert (-1 to exit) :     6
+Enter data to insert (-1 to exit) :     7
+Enter data to insert (-1 to exit) :     -1
+
+4 2 1 3 6 5 7
+*/
